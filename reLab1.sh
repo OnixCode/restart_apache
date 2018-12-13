bac627aa48e1e1af4fe388e272e79679341bdf48
John Falzone <21jroc@gmail.com>
2018-12-13 10:15:23 -0600

    Reset test file for lab.

--- reLab1.sh
+++ reLab1.sh
@@ -2,35 +2,12 @@
 
 CONFIG="$1"
 COMMAND="$2"
-BOOLEAN="false"
 
 if [ $# -ne 2 ]
 then
     echo "ERROR: $0 requires two parameters {virtual-host} {restart|reload}"
     exit 1
 fi
-
-# List all of the configuration files in the _/etc/apache2/sites-available/_ directory
-VHOSTS_PATH=/etc/apache2/sites-available/*.conf 
-
-for FILENAME in ${VHOSTS_PATH}
-do  
-     echo $PATHNAME
-        if [ "$CONFIG" == "${PATHNAME}" ]
-        then
-            BOOLEAN="true"
-            echo $BOOLEAN
-        fi
-done
-
-
-if [ $BOOLEAN == "false" ]
-then    echo "ERROR: "$CONFIG" is not a valid virtual-host file."
-        for FILENAME in ${VHOSTS_PATH}
-        do echo ${FILENAME:29}
-        done
-fi
-exit 1
 
 if [ "$COMMAND" == "reload" ] || [ "$COMMAND" == "restart" ]
 then
