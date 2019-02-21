c5a7fcba13c3fb2273567645d8655be483fb214b
John Falzone <21jroc@gmail.com>
2019-02-20 19:11:54 -0600

    Added the ability to specify virtual hosts and service command

--- re.sh
+++ re.sh
@@ -5,39 +5,17 @@ COMMAND="$2"
 
 if [ $# -ne 2 ]
 then
-    echo "ERROR: $0 requires two parameters {virtual-host} {restart|reload}"
+    echo "$0 requires two parameters {virtual-host} {restart|reload}"
     exit 1
 fi
 
-# List all of the configuration files in the _/etc/apache2/sites-available/_ directory
-VHOSTS_PATH=/etc/apache2/sites-available/*.conf 
+#Move the current execution state to the proper directory
+cd /etc/apache2/sites-available
 
-for FILENAME in ${VHOSTS_PATH}
-do  
-    PATHNAME=${FILENAME:29:-5}
-        if [ "$CONFIG" != "${PATHNAME}" ]
-        then
-            echo ${FILENAME:29:-5}   
-        fi
-done
-echo "ERROR: "$CONFIG" is not a valid virtual-host file."
-exit 1
+#Disable a vhost configuration
+sudo a2dissite "$CONFIG"
+sudo service apache2 "$COMMAND"
 
-if [ "$COMMAND" == "reload" ] || [ "$COMMAND" == "restart" ]
-then
-    # Move the current execution state to the proper directory
-    cd /etc/apache2/sites-available
-
-    # Disable a vhost configuration
-    sudo a2dissite "$CONFIG"
-    sudo service apache2 "$COMMAND"
-
-    # Enable a vhost configuration
-    sudo a2ensite "$CONFIG"
-    sudo service apache2 "$COMMAND"
-
-else 
-    echo "ERROR: $COMMAND is not a valid service directive {reload|restart}"
-    exit 1
-fi
-
+#Enable a vhost configuration
+sudo a2ensite "$CONFIG"
+sudo service apache2 "$COMMAND"
